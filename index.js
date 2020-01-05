e33eeae0f01f8257d8951312227da1d6d837f59d
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-01-05 05:01:19 +0300

    Ders 5:index.js dosyasının oluşturulması ve  mongoDB'ye bağlanılması

---
+++ index.js
@@ -0,0 +1,36 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
+let cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+let port = process.env.port
+let dbcon = process.env.cloud_mongodb_con;
+
+let app = new express();
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json());
+
+mongoose.connect(dbcon, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+var con = mongoose.connection;
+
+if (!con) console.log("mongoDB'ye bağlanılamadı.")
+else {
+    console.log("mongoDB'ye başarıyla bağlanıldı")
+}
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+app.listen(port, () => {
+    console.log("node.js server çalışıyor...")
+})
