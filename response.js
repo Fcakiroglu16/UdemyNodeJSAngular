b8c26e2a6e6a87e6b551feabab6feca4f1f4ca6d
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-01-05 05:46:52 +0300

    Ders 8 :Response.js dosyasının oluşturulması

---
+++ response.js
@@ -0,0 +1,44 @@
+class response {
+
+    constructor(data = null, errors = null) {
+        this.errors = errors;
+        this.data = data;
+    }
+    success(res) {
+        return res.status(200).json({
+            status: "success",
+            data: this.data
+        })
+    }
+
+    created(res) {
+
+        return res.status(201).json({
+            status: "crated",
+            data: this.data
+        })
+
+    }
+    error500(res) {
+        res.status(500).json({
+            status: "error",
+            errors: this.errors
+        })
+
+    }
+    error400(res) {
+        res.status(400).json({
+            status: "error",
+            errors: this.errors
+        })
+
+    }
+
+    notFound(res) {
+        res.status(404).json({
+            status: "not found",
+            errors: ""
+        })
+
+    }
+}
