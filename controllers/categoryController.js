58736d741160f945d2f5e57ec5c28190a1f347b1
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-01-05 06:18:37 +0300

    Ders 10:getById() endpoint'inin kodlanması

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,6 +2,7 @@ Category = require("../models/category.model");
 
 response = require("../response");
 
+//GET http://localhost/api/category
 exports.list = (req, res) => {
 
     Category.find({}, (err, categories) => {
@@ -10,12 +11,21 @@ exports.list = (req, res) => {
         }
 
         return new response(categories, null).success(res);
-
-
-
-
     })
 
+    //GET http://localhost/api/category/84023804923
 
+}
+
+exports.getById = (req, res) => {
+
+    Category.findById(req.params.category_id, (err, category) => {
+
+        if (err) {
+            return new response(null, err).notFound(res);
+        }
+        return new response(category, null).success(res);
+
+    })
 
 }
