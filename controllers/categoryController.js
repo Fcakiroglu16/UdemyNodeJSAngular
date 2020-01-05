8598756494bf80daff6760d6fedc85b43a6261d6
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-01-05 06:27:20 +0300

    Ders 11:create() endpoint'inin kodlanması

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -13,10 +13,10 @@ exports.list = (req, res) => {
         return new response(categories, null).success(res);
     })
 
-    //GET http://localhost/api/category/84023804923
+
 
 }
-
+//GET http://localhost/api/category/84023804923
 exports.getById = (req, res) => {
 
     Category.findById(req.params.category_id, (err, category) => {
@@ -27,5 +27,26 @@ exports.getById = (req, res) => {
         return new response(category, null).success(res);
 
     })
+
+}
+
+exports.create = (req, res) => {
+
+
+    var category = new Category();
+    category.name = req.body.name;
+
+    category.save((err) => {
+
+        if (err)
+            return new response(null, err).error500(res);
+
+        return new response(category, null).created(res);
+
+
+
+
+    })
+
 
 }
