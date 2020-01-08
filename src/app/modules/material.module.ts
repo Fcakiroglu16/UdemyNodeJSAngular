e41077083e5735fdab9dca28aee12286a2d7a673
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-01-08 12:57:41 +0300

    Ders 47:Ana sayfada tüm kitapların listelenmesi

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -9,7 +9,8 @@ import {
   MatPaginatorModule,
   MatTableModule,
   MatIconModule,
-  MatAutocompleteModule
+  MatAutocompleteModule,
+  MatCardModule
 } from "@angular/material";
 
 @NgModule({
@@ -24,7 +25,8 @@ import {
     MatPaginatorModule,
     MatTableModule,
     MatIconModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    MatCardModule
   ],
   exports: [
     MatToolbarModule,
@@ -35,7 +37,8 @@ import {
     MatPaginatorModule,
     MatTableModule,
     MatIconModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    MatCardModule
   ]
 })
 export class MaterialModule {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BookService } from "src/app/services/book.service";
+import { Book } from "src/app/models/book";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private bookService: BookService) {}
+  books: Book[];
   ngOnInit() {
+    this.bookService.getBooks().subscribe(result => {
+      this.books = result;
+    });
   }
-
 }
